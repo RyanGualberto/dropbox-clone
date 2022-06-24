afb4e12e538b803a7be4115f4d13921d84ffeb83
Ryan Gualberto <RyanOliveiraGualberto@gmail.com>
2022-06-24 08:26:41 -0300

    aula 92: add: httpRequest - ajax

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -1,12 +1,12 @@
 class DropBoxController {
-    constructor () {
+    constructor() {
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector('#react-snackbar-root')
         this.initEvents();
     }
 
-    initEvents(){
+    initEvents() {
         this.btnSendFileEl.addEventListener("click", event => {
             this.inputFilesEl.click();
         });
@@ -17,7 +17,30 @@ class DropBoxController {
         });
     }
 
-    uploadTask(files){
+    uploadTask(files) {
         let promises = [];
+
+        [...files].forEach(file => {
+            promises.push(new Promise((resolve, reject) => {
+                let ajax = new XMLHttpRequest();
+                ajax.open('POST', '/upload');
+                ajax.onload = event => {
+                    try {
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch (e) {
+                        reject(e);
+                    }
+                };
+                ajax.onerror = event => {
+                    reject(event);
+                };
+
+                let formData = new FormData();
+                formData.append('input-file', file);
+                ajax.send(formData);
+            }));
+        });
+
+        return Promise.all(promises)
     }
 }
